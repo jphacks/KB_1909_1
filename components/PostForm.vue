875f828cfaae6fc1576484980d65271e0b304930
aratasato <14242642+aratasato@users.noreply.github.com>
2019-10-19 14:22:48 +0900

    post form のマークアップ

---
+++ components/PostForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <p>何でも投稿してください！</p>
+    <v-textarea></v-textarea>
+    <v-btn right absolute outlined></v-btn>
+  </v-container>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+class PostForm extends Vue {}
+
+export default PostForm
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
     <v-app-bar>monu</v-app-bar>
+    <post-form></post-form>
   </div>
 </template>
 
 <script>
 import { Component, Vue } from 'vue-property-decorator'
+import PostForm from '@/components/PostForm'
 
-@Component({})
+@Component({
+  components: {
+    PostForm
+  }
+})
 class Index extends Vue {}
 
 export default Index
