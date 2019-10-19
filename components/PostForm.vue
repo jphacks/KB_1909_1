739e60156fe7cf3432555f89bc061fd509c6a248
ataran <densiarata2@gmail.com>
2019-10-19 16:02:23 +0900

    post formのマークアップ (#2)
    * post form のマークアップ
    
    * 細かい修正

---
+++ components/PostForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <p>何でも投稿してください！</p>
+    <v-textarea v-model="postBody"></v-textarea>
+    <v-btn right absolute outlined>投稿</v-btn>
+  </v-container>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+class PostForm extends Vue {
+  postBody = ''
+}
+
+export default PostForm
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,28 @@
 <template>
   <div>
-    <v-app-bar>monu</v-app-bar>
+    <v-app-bar>
+      <h1>monu</h1>
+    </v-app-bar>
+    <post-form></post-form>
   </div>
 </template>
 
 <script>
 import { Component, Vue } from 'vue-property-decorator'
+import PostForm from '@/components/PostForm'
 
-@Component({})
+@Component({
+  components: {
+    PostForm
+  }
+})
 class Index extends Vue {}
 
 export default Index
 </script>
+
+<style lang="scss">
+h1 {
+  text-align: center;
+}
+</style>
